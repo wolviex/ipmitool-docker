146f25439628f61646caa5bb4cda32785d627ff4
Dennis Schridde <dennis.schridde@uni-heidelberg.de>
2018-09-20 16:30:50 +0200

    Implement an EAPI and new Manifest format
    * Manifest now includes the file size
    * pkg.sh can now generate SOURCE entries in Manifest
    * The EAPI env var determines which default implementation of src_* functions
      to use

---
+++ build/pkg-api-1.sh
@@ -0,0 +1,48 @@
+S="${WORKDIR}/${P}"
+B="${S}_build"
+
+src_fetch() {
+	local uri file
+	cd "${CACHEDIR}"
+	for uri in "${SOURCES[@]}" ; do
+		file="${uri##*/}"
+		if ! verify_source "${file}" ; then
+			curl --fail --silent --show-error --location --output "${file}" "${uri}"
+		fi
+	done
+}
+
+src_unpack() {
+	local uri file
+	cd "${WORKDIR}"
+	for uri in "${SOURCES[@]}" ; do
+		file="${CACHEDIR}/${uri##*/}"
+		case "${file}" in
+			*.gz) tar -xzf "${file}" ;;
+			*.bz2) tar -xjf "${file}" ;;
+			*.xz) tar -xJf "${file}" ;;
+			*) die "Unsupported file format: ${file}" ;;
+		esac
+	done
+}
+
+src_prepare() {
+	cd "${S}"
+	epatch
+}
+
+src_configure() {
+	mkdir "${B}"
+	cd "${B}"
+	"${S}"/configure
+}
+
+src_compile() {
+	cd "${B}"
+	make
+}
+
+src_install() {
+	cd "${B}"
+	make install DESTDIR="${D}"
+}
--- build/pkg.sh
+++ build/pkg.sh
@@ -6,7 +6,8 @@ USAGE="$0 <command>"
 USAGE_build="$0 build <package>"
 
 die() {
-	echo "$@"
+	local error="${*:-(unknown)}"
+	echo "Error: ${error} in ${FUNCNAME[1]}() at ${BASH_SOURCE[1]}:${BASH_LINENO[0]}" > /dev/stderr
 	exit 1
 }
 
@@ -18,18 +19,53 @@ has() {
 	return 1
 }
 
-verify() {
-	while read type sum file ; do
+verify_source() {
+	local MANIFEST="$1" ; shift
+	[[ "${MANIFEST}" ]] || die
+
+	local SOURCE="$1" ; shift
+	if [[ "${SOURCE}" ]] ; then
+		if ! test -f "${SOURCE}" ; then
+			return 1
+		fi
+
+		while read type file size sha256sum ; do
+			case "${type}" in
+				SOURCE)
+					if [[ "${file}" = "${SOURCE}" ]] ; then
+						actual_size=$(file_size "${file}")
+						if [[ "${actual_size}" -ne "${size}" ]] ; then
+							return 1
+						elif ! echo "${sha256sum}  ${CACHEDIR}/${file}" | sha256sum -c ; then
+							return 1
+						fi
+					fi
+					;;
+			esac
+		done < "${MANIFEST}"
+
+		return 0
+	fi
+
+	local uri file
+	for uri in "${SOURCES[@]}" ; do
+		file="${CACHEDIR}/${uri##*/}"
+		verify_source "${MANIFEST}" "${file}"
+	done
+}
+
+verify_manifest() {
+	while read type file size sha256sum ; do
 		case "${type}" in
 			AUX)
-				echo "${sum}  ${AUXDIR}/${file}" | sha256sum -c
+				echo "${sha256sum}  ${AUXDIR}/${file}" | sha256sum -c
 				;;
 			SCRIPT)
-				echo "${sum}  ${SCRIPTDIR}/${file}" | sha256sum -c
+				echo "${sha256sum}  ${SCRIPTDIR}/${file}" | sha256sum -c
 				;;
 			SOURCE)
 				if has "${file}" "${SOURCES[@]}" ; then
-					echo "${sum}  ${SOURCEDIR}/${file}" | sha256sum -c
+					echo "${sha256sum}  ${CACHEDIR}/${file}" | sha256sum -c
 				fi
 				;;
 			*)
@@ -54,19 +90,37 @@ pkg_install() {
 	rsync -av "${D}/" "${ROOT}/"
 }
 
-build() {
-	[[ "$1" ]] || die "${USAGE_build}"
+split_package_name_version() {
 	P="$1" ; shift
+	test -n "${P}" || die
 
 	PN="${P%%-[0-9]*}"
 	PV="${P##${PN}-}"
+}
 
-	SCRIPTFILE="${SCRIPTDIR}/${P}.build"
+set_package_env() {
+	local SCRIPTFILE="$1" ; shift
+	test -n "${SCRIPTFILE}" || die
+
+	P="$(basename ${SCRIPTFILE} .build)"
+	split_package_name_version "${P}"
+}
+
+build() {
+	P="$1" ; shift
+	test -n "${P}" || die "${USAGE_build}"
+
+	split_package_name_version "${P}"
+
+	local SCRIPTFILE="${SCRIPTDIR}/${P}.build"
+	test -f "${SCRIPTFILE}" || die "${SCRIPTFILE} not found!"
 
 	source "${SCRIPTFILE}"
+	test -n "${EAPI}" || die "${SCRIPTFILE} needs to set EAPI!"
+	source "${MYDIR}/pkg-api-${EAPI}.sh"
 
 	src_fetch
-	verify
+	verify_manifest
 	src_unpack
 	src_prepare
 	set -x
@@ -77,36 +131,85 @@ build() {
 	pkg_install
 }
 
+file_size() {
+	local file="$1" ; shift
+	du --bytes "${file}" | cut -d$'\t' -f1
+}
+
+file_sha256sum() {
+	local file="$1" ; shift
+	sha256sum "${file}" | cut -d' ' -f1
+}
+
+sources_manifest() {
+	local MANIFESTFILE="$1" ; shift
+	test -f "${MANIFESTFILE}" || die
+
+	local SCRIPTFILE="$1" ; shift
+	test -f "${SCRIPTFILE}" || die
+
+	set_package_env "${SCRIPTFILE}"
+
+	source "${SCRIPTFILE}"
+	test -n "${EAPI}" || die "${SCRIPTFILE} needs to set EAPI!"
+	source "${MYDIR}/pkg-api-${EAPI}.sh"
+
+	src_fetch
+
+	local uri file
+	for uri in "${SOURCES[@]}" ; do
+		file="${CACHEDIR}/${uri##*/}"
+		size=$(file_size "${file}")
+		sha256sum=$(file_sha256sum "${file}")
+		echo "SOURCE  $(basename ${file})  ${size}  ${sha256sum}" >> "${MANIFESTFILE}"
+	done
+}
+
 manifest() {
 	local MANIFESTFILE="${SCRIPTDIR}"/Manifest
 
 	sed -i "${MANIFESTFILE}" \
 		-e '/^AUX  /d' \
-		-e '/^SCRIPT  /d'
+		-e '/^SCRIPT  /d' \
+		-e '/^SOURCE  /d'
 
-	cd "${SCRIPTDIR}"
-	for f in *.build ; do
-		sha256sum "${f}" | sed -e 's:^:SCRIPT  :' >> "${MANIFESTFILE}"
+	for f in "${SCRIPTDIR}"/*.build ; do
+		if ! test -f "${f}" ; then
+			break
+		fi
+
+		size=$(file_size "${f}")
+		sha256sum=$(file_sha256sum "${f}")
+		echo "SCRIPT  $(basename ${f})  ${size}  ${sha256sum}" >> "${MANIFESTFILE}"
+		sources_manifest "${MANIFESTFILE}" "${f}"
 	done
 
-	cd "${AUXDIR}"
-	for f in * ; do
-		sha256sum "${f}" | sed -e 's:^:AUX  :' >> "${MANIFESTFILE}"
+	for f in "${AUXDIR}"/* ; do
+		if ! test -f "${f}" ; then
+			break
+		fi
+
+		size=$(file_size "${f}")
+		sha256sum=$(file_sha256sum "${f}")
+		echo "AUX  $(basename ${f})  ${size}  ${sha256sum}" >> "${MANIFESTFILE}"
 	done
 }
 
 command() {
-	[[ "$1" ]] || die "${USAGE}"
 	local command="$1" ; shift
 
 	case "${command}" in
 		build) build "$@" ;;
 		manifest) manifest "$@" ;;
+		*) die "${USAGE}" ;;
 	esac
 }
 
+CACHEDIR="${HOME}/.cache/tinypkg"
 MYDIR="$(cd $(dirname $0) && echo ${PWD})"
 SCRIPTDIR="${MYDIR}/script"
 AUXDIR="${SCRIPTDIR}/aux"
+
+mkdir -p "${CACHEDIR}"
 
 command "$@"
